421dfc8922c8b69a32782d2b6627012f8e256b66
Ryan <rshtirmer@gmail.com>
2022-03-04 16:15:34 -0500

    Init quorum

--- src/modules/configuration/index.ts
+++ src/modules/configuration/index.ts
@@ -3,12 +3,15 @@ export default () => ({
     uri: process.env.MONGODB_URI,
   },
   port: process.env.APP_PORT,
+  alchemy_token: process.env.ALCHEMY_TOKEN,
   app_env: process.env.APP_ENV,
   ethereum_network: process.env.ETHEREUM_NETWORK,
-  session_secret: process.env.SESSION_SECRET,
+  ethereum_quorum: process.env.ETHEREUM_QUORUM,
   infura: {
     project_id: process.env.INFURA_PROJECT_ID,
+    project_secret: process.env.INFURA_PROJECT_SECRET,
   },
+  session_secret: process.env.SESSION_SECRET,
   etherscan_api_key: process.env.ETHERSCAN_API_KEY,
   recent_block_gap: process.env.RECENT_BLOCK_GAP,
 });
